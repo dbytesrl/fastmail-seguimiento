9a2febc31a1928b06b5086e8feed6d7969169b56
bertuja <bertuja@gmail.com>
2026-05-13 15:32:46 -0300

    Fix Clientify WhatsApp widget: use dynamic script creation instead of document.write per Clientify documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,16 @@ export default function RootLayout({
       <body className="antialiased bg-white text-fastmail-dark">
         {children}
         <Script src="https://api.clientify.net/web-marketing/webforms/script/145890.js" strategy="afterInteractive" />
-        <Script
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `document.write('<script async>(function (w, d) { setTimeout(() => {var iwapp = d.createElement("whatsApp-root");d.getElementsByTagName("body")[0].appendChild(iwapp);w.Clientify_WhatsappBID = "168100";w.Clientify_WhatsappUID = "27235";var h = d.head || d.getElementsByTagName("head")[0];var s = d.createElement("script");s.setAttribute("type", "text/javascript");s.setAttribute("src", "https://api.clientify.net/static/js/whatsapp-widget/embed-whatsapp/embed.js");h.appendChild(s);}, 1000)})(window, document);</script>')`,
-          }}
-        />
+        <Script id="clientify-whatsapp" strategy="afterInteractive">
+          {`
+            (function() {
+              var script = document.createElement('script');
+              script.async = true;
+              script.innerHTML = '(function (w, d) { setTimeout(() => {var iwapp = d.createElement("whatsApp-root");d.getElementsByTagName("body")[0].appendChild(iwapp);w.Clientify_WhatsappBID = "168100";w.Clientify_WhatsappUID = "27235";var h = d.head || d.getElementsByTagName("head")[0];var s = d.createElement("script");s.setAttribute("type", "text/javascript");s.setAttribute("src", "https://api.clientify.net/static/js/whatsapp-widget/embed-whatsapp/embed.js");h.appendChild(s);}, 1000)})(window, document);';
+              document.body.appendChild(script);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
